fb00c7a36f29c9932c6d9464b9fa8c30eb7bf8ef
Nathmon565 <nathmon565@gmail.com>
2020-08-15 12:49:32 -0700

    Start Project
    Import unity files and add some basic starter code.
    The dungeon generates with empty tiles.
    The other tiles are present, just not used.

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>Handles the overall generation of the dungeon</summary>
+public class DungeonGenerator : MonoBehaviour {
+    [Header("Dungeon Settings")]
+    ///<summary>How many tiles the dungeon spans along the world x and z axis</summary>
+    public Vector2 dungeonDimensions = new Vector2(10, 10);
+    ///<summary>The list of tile prefabs, to be instantiated</summary>
+    public List<GameObject> tiles;
+    ///<summary>The default tile to be placed when creating the dungeon</summary>
+    public GameObject defaultTile;
+    public GameObject tileRoot;
+    [Space(10)]
+
+    ///<summary>Force the dungeon to generate</summary>
+    public bool generateDungeon = false;
+
+    [Header("Current Dungeon")]
+    ///<summary>An empty object to hold all of the tiles and details of the dungeon</summary>
+    public GameObject dungeonRoot;
+    ///<summary>The list of instantiated prefabs, currently present in the world</summary>
+    public List<GameObject> dungeonTiles;
+
+    // Start is called before the first frame update
+    private void Start() {
+        //Create tileRoot if it doesn't exist
+        if(tileRoot == null) {
+            tileRoot = new GameObject();
+            tileRoot.transform.position = Vector3.zero;
+            tileRoot.name = "Tile Root";
+        }
+
+        //For each tile, optimize it so it is faster to instantiate
+        for(int i = 0; i < tiles.ToArray().Length; i++) {
+            tiles[i] = OptimizeTile(tiles[i], tileRoot.transform, (i + 1) * 10);
+        }
+        defaultTile = OptimizeTile(defaultTile, tileRoot.transform, 0);
+
+        tileRoot.SetActive(false);
+
+        //Generate the dungeon with the current settings
+        //once the scene is launched
+        GenerateDungeon();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if(generateDungeon) {
+            GenerateDungeon();
+            generateDungeon = false;
+        }
+    }
+
+    ///<summary>Generate the dungeon with the current setup</summary>
+    public void GenerateDungeon() {
+        Debug.Log("Generating the dungeon...\nWidth: " + dungeonDimensions.x + ", Length: " + dungeonDimensions.y + ", Tiles: " + (dungeonDimensions.x * dungeonDimensions.y));
+        float timeStart = Time.realtimeSinceStartup;
+        //If the dungeon has been generated before
+        if(dungeonRoot != null) {
+            //Destroy the dungeon, and reset the list
+            Destroy(dungeonRoot);
+            dungeonTiles = new List<GameObject>();
+        }
+        //Create a new root at (0,0,0)
+        dungeonRoot = new GameObject();
+        dungeonRoot.transform.position = Vector3.zero;
+        dungeonRoot.name = "Dungeon Root";
+        
+        //Create the list of empty tiles
+        for(int x = 0; x < dungeonDimensions.x; x++) {
+            for(int y = 0; y < dungeonDimensions.y; y++) {
+                //Create an empty tile, and add it to the list of tiles
+                dungeonTiles.Add(Instantiate(defaultTile, new Vector3(x * 10, 0, y * 10), Quaternion.Euler(Vector3.zero), dungeonRoot.transform));
+                //dungeonTiles.Add(Instantiate(tiles[Random.Range(0, tiles.ToArray().Length)], new Vector3(x * 10, 0, y * 10), Quaternion.Euler(Vector3.zero), dungeonRoot.transform));
+            }
+        }
+
+
+        Debug.Log("Dungeon generation complete! Total time taken: " + (Mathf.Round((Time.realtimeSinceStartup - timeStart) * 100000) / 100f) + "ms (" + (Mathf.Round((Time.realtimeSinceStartup - timeStart) * 100) / 100f) + " seconds)");
+    }
+
+    ///<summary>Create an instantiated and prepared version of the tile to be duplicated</summary>
+    ///<param name="tile">The tile to be optimized</param>
+    ///<param name="posModifier">The x coordinate modifier to esnure the raycast is not blocked</param>
+    public GameObject OptimizeTile(GameObject tile, Transform parent, float posModifier) {
+        GameObject newTile = Instantiate(tile, new Vector3(posModifier, 0, 0), Quaternion.Euler(Vector3.zero), parent);
+        StartCoroutine(WaitUntilReady(newTile.GetComponent<DungeonTile>()));
+        return newTile;
+    }
+
+    ///<summary>Waits until the tile's readyToUse boolean is true</summary>
+    ///<param name="tile">The tile to check</param>
+    private IEnumerator WaitUntilReady(DungeonTile tile) {
+        yield return new WaitUntil(() => tile.readyToUse);
+    }
+}
---
+++ Assets/Scripts/DungeonTile.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>Defines how this tile can be placed in relation to other tiles</summary>
+public class DungeonTile : MonoBehaviour {
+
+    ///<summary>A class containing the booleans which determine what directions this
+    /// tile can connect to</summary>
+    [System.Serializable]
+    public class AvailableDirections {
+        ///<summary>Whether this tile can connect on the local +X axis</summary>
+        public bool localXPos;
+        ///<summary>Whether this tile can connect on the local -X axis</summary>
+        public bool localXNeg;
+        ///<summary>Whether this tile can connect on the local +Z axis</summary>
+        public bool localZPos;
+        
+        ///<summary>Whether this tile can connect on the local -Z axis</summary>
+        public bool localZNeg;
+    }
+
+    public bool readyToUse = false;
+    public AvailableDirections directions = new AvailableDirections();
+    
+    ///<summary>How many faces each sub mesh has</summary>
+    private int[] subMeshesFaceTotals;
+    ///<summary>How many sub meshes there are</summary>
+    private int totalSubMeshes;
+    
+    // Awake is called before Start()
+    private void Awake() {
+        //Credit to Damien O'Connell at
+        //https://forum.unity.com/threads/detecting-material-material-index-and-raycast-hit.40377/
+        //for detecting material from a raycast hit
+
+        //Determine how many subMeshes and faces this has
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        
+        //Get the number of sub meshes on this object
+        totalSubMeshes = mesh.subMeshCount;
+        
+        //Set up the array to the number of sub meshes
+        subMeshesFaceTotals = new int[totalSubMeshes];
+
+        //For each mesh...
+        for(int i = 0; i < totalSubMeshes; i++) {
+            //Calculate the number of faces on this mesh
+            subMeshesFaceTotals[i] = mesh.GetTriangles(i).Length / 3;
+        }
+        
+        //Figure out which directions this tile can connect to
+        SetDirectionAvailability();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    ///<summary>Set the available direction booleans based on the materials of the model at the cardinal directions</summary>
+    public void SetDirectionAvailability() {
+        //Debug.Log("Setting direction availability of tile " + name);
+        
+        
+        //Check and set the direction availability for each direction
+        directions.localXPos = CheckRayCastDirection(new Vector3( 4.9f, 1, 0));
+        directions.localXNeg = CheckRayCastDirection(new Vector3(-4.9f, 1, 0));
+        directions.localZPos = CheckRayCastDirection(new Vector3(0, 1,  4.9f));
+        directions.localZNeg = CheckRayCastDirection(new Vector3(0, 1, -4.9f));
+
+        readyToUse = true;
+    }
+
+    ///<summary>Sets the boolean according to originOffset and what the raycast hits</summary>
+    ///<param name="originOffset">The local position that the raycast should originate from. Also used to determine which bool to change</param>
+    private bool CheckRayCastDirection(Vector3 originOffset) {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.TransformPoint(originOffset), -transform.up, out hit, 5)) {
+            //Credit to Damien O'Connell at
+            //https://forum.unity.com/threads/detecting-material-material-index-and-raycast-hit.40377/
+            //for detecting material from a raycast hit
+
+            //Which sub mesh we hit
+            int hitSubMeshNumber = 0;
+            //The maximum number we can hit on the current submesh
+            int maxVal = 0;
+            
+            //For each submesh...
+            for(int i = 0; i < totalSubMeshes; i++) {
+                //Add the number of triangles present
+                maxVal += subMeshesFaceTotals[i];
+                //If we are in range...
+                if(hit.triangleIndex <= maxVal - 1) {
+                    //Set the subMesh that we hit
+                    hitSubMeshNumber = i + 1;
+                    //Leave the for loop
+                    break;
+                }
+                //Otherwise, try again
+            }
+
+            //Debug.Log("Hitting triangle " + hit.triangleIndex + "\nSubmesh " + hitSubMeshNumber);
+            //If we hit the gray material (wall)
+            if(hitSubMeshNumber == 1) {
+                return false;
+            } else {
+                //We hit the white material (walkway)
+                return true;
+            }
+        } else {
+            Debug.LogError("Raycast failed when setting direction availability " + originOffset + " - It didn't hit anything.");
+            return false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Debug rays for visibility in the editor
+        /*
+        Debug.DrawRay(transform.TransformPoint(new Vector3(4.9f, 1, 0)), -transform.up, Color.red, Time.deltaTime);
+        Debug.DrawRay(transform.TransformPoint(new Vector3(-4.9f, 1, 0)), -transform.up, Color.yellow, Time.deltaTime);
+        Debug.DrawRay(transform.TransformPoint(new Vector3(0, 1, 4.9f)), -transform.up, Color.blue, Time.deltaTime);
+        Debug.DrawRay(transform.TransformPoint(new Vector3(0, 1, -4.9f)), -transform.up, Color.magenta, Time.deltaTime);
+        */
+    }
+}
